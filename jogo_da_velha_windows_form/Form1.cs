e4802085f615df1f6b5dc835c093918a3fbf97e5
LucasArusiewicz <lucasarusiewicz@gmail.com>
2018-04-20 01:16:19 -0300

    Upload do projeto

---
+++ jogo_da_velha_windows_form/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace jogo_da_velha_windows_form
+{
+    public partial class Form1 : Form
+    {
+
+        Velha jogo = new Velha();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NovoJogo();
+        }
+
+        private void NovoJogo()
+        {
+            //Limpa Valores dos botoes
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            jogo.NovoJogo();
+
+            MessageBox.Show("Jogador 1 = X\nJogador 2 = O", "INFO JOGADORES", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Jogada(Button botao, int[] casa)
+        {
+            string jogador = jogo.Jogador1 ? "X" : "O";
+
+            if (jogo.DefineCasa(casa))
+            {
+                botao.Text = jogador;
+                switch (jogo.ProximaJogada())
+                {
+                    case 1:
+                        MessageBox.Show("JOGADOR 1\nJogador X", "VENCEDOR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        NovoJogo();
+                        break;
+                    case 2:
+                        MessageBox.Show("JOGADOR 2\nJogador O", "VENCEDOR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        NovoJogo();
+                        break;
+                    case 3:
+                        MessageBox.Show("EMPATE", "VELHA", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        NovoJogo();
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Essa casa já foi jogada.\nJogue em outra casa.", "CASA INVÁLIDA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Jogada(button1, new int [2]{ 0, 0 });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Jogada(button2, new int[2] { 0, 1 });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Jogada(button3, new int[2] { 0, 2 });
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Jogada(button4, new int[2] { 1, 0 });
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Jogada(button5, new int[2] { 1, 1 });
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Jogada(button6, new int[2] { 1, 2 });
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Jogada(button7, new int[2] { 2, 0 });
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Jogada(button8, new int[2] { 2, 1 });
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Jogada(button9, new int[2] { 2, 2 });
+        }
+
+        private void button_novoJogo_Click(object sender, EventArgs e)
+        {
+            NovoJogo();
+        }
+
+    }
+}
---
+++ jogo_da_velha_windows_form/Velha.cs
@@ -0,0 +1,115 @@
+﻿namespace jogo_da_velha_windows_form
+{
+    class Velha
+    {
+        //Jogador 1 = inteiro de valor 1
+        //Jogador 2 = inteiro de valor 2
+
+        //Atributos
+        int[,] campo = new int[3, 3];
+        bool jogador1 = true;
+        int contJogadas = 0;
+
+        public bool Jogador1 { get => jogador1;}
+
+        public int ProximaJogada()
+        {
+            contJogadas++;
+
+            int valorJogadorAtual = jogador1 ? 1 : 2;
+
+            switch (VerificaVencedor(valorJogadorAtual))
+            {
+                case 0:
+                    if (contJogadas == 9)
+                    {
+                        return 3;
+                    }
+                    break;
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+            }
+
+            jogador1 = !jogador1;
+            return 0;
+        }
+
+        public int VerificaVencedor(int jogador)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (VerificaLinha(i, jogador)) return jogador;
+                if (VerificaColuna(i, jogador)) return jogador;
+            }
+
+            if (VerificaDiagonal(jogador))
+            {
+                return jogador;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private bool VerificaLinha(int linha, int jogador)
+        {
+            if(campo[linha, 0] == jogador && campo[linha, 1] == jogador && campo[linha, 2] == jogador)
+            {
+                return true;
+            }               
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool VerificaColuna(int coluna, int jogador)
+        {
+            if (campo[0, coluna] == jogador && campo[1, coluna] == jogador && campo[2, coluna] == jogador)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool VerificaDiagonal(int jogador)
+        {
+            if ((campo[0, 0] == jogador && campo[1, 1] == jogador && campo[2, 2] == jogador) || (campo[0, 2] == jogador && campo[1, 1] == jogador && campo[2, 0] == jogador))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void NovoJogo()
+        {
+            campo = new int[3, 3];
+            contJogadas = 0;
+            jogador1 = true;
+        }
+
+        public bool DefineCasa(int[] jogada)
+        {
+            int valorCasa = campo[jogada[0], jogada[1]];
+
+            if(valorCasa != 0)
+            {
+                return false;
+            }
+            else
+            {
+                campo[jogada[0], jogada[1]] = jogador1 ? 1 : 2;
+                return true;
+            }
+        }
+    }
+}
